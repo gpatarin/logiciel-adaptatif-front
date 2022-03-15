3d6966d394c430c08d1aa2ff8c6bb061283a017d
DevilDevShark <kevin.roland.pro@outlook.fr>
2022-03-15 09:39:40 +0100

    [Add]: modif

---
+++ src/Client.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { idText } from 'typescript';
+
+class clients {
+    state = {
+        name: idText,
+        type: String,
+        firsName: String,
+        lastName: String,
+        mail:String,
+        createdAt: Date,
+    }
+
+    list = {
+        
+    }
+}
+
+const Client = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Client;
---
+++ src/product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createSemanticDiagnosticsBuilderProgram, ObjectFlags } from 'typescript';
+
+class produits {
+    state = {
+        id: String,
+        name: String,
+        stock: Int32Array,
+        image: image,
+        price: double
+    }
+}
+
+const product = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+export default product;
